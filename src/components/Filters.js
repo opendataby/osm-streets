ae5d8f347c5d0d05eae34493a13f39dc1f88b0a4
Pavel Tyslacki <pavel.tyslyatsky@gmail.com>
2016-04-25 22:12:11 +0300

    add statistic

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -10,6 +10,19 @@ export default class Filters extends Component {
     data: PropTypes.object,
   }
 
+  calculateStats (data, streetIds) {
+    let streetsLength = 0
+    let cities = {}
+
+    for (let streetId of streetIds) {
+      let street = data.results[streetId]
+      streetsLength += street.l
+      cities[street.c] = true
+    }
+
+    return {streetsLength, streetsCount: streetIds.length, citiesCount: Object.keys(cities).length}
+  }
+
   render () {
     const {data, filters, backMap} = this.props
 
@@ -17,9 +30,17 @@ export default class Filters extends Component {
       return null
     }
 
+    let stats = this.calculateStats(data, Object.keys(data.results))
+
     let tr = translate.bind(null, data.translates)
 
     return <div className={ classNames('filters', {'back-map': backMap})}>
+      <div className='filter-stats'>
+        { stats.streetsCount } { tr('streets') }&nbsp;
+        { tr('with summary length') } { stats.streetsLength } { tr('m') }&nbsp;
+        { tr('in') } { stats.citiesCount } { tr('cities') }
+      </div>
+
       <div className='filter'>
         <input className='filter-add' type='text' placeholder={ tr('type filter name') }/>
       </div>
