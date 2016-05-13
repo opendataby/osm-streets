a5321119f5c62e992539508129b78a8fa45b3eb2
Pavel Tyslacki <pavel.tyslyatsky@gmail.com>
2016-05-13 23:05:14 +0300

    remove low quality languages

--- src/constants.js
+++ src/constants.js
@@ -24,8 +24,6 @@ export const TILE_OPTIONS = {
 export const DEFAULT_LANGUAGE = 'be-tarask'
 export const LANGUAGES = {
   'be-tarask': 'be-tarask',
-  'be': 'be',
-  'ru': 'ru',
   'en': 'en',
 }
 
